ba9acaf757c4c07fea1dca8ed6bd95a0ebb55c0b
ashleyzhou972 <ashley.n.zhou@gmail.com>
2016-07-18 16:48:34 -0500

    restructure

--- precRec.py
+++ precRec.py
@@ -9,18 +9,10 @@ dependency: -
 """
 
 
-
-import os
 import sys
-import numpy
-import matplotlib.pyplot as plt
-from collections import defaultdict
-#os.chdir('/home/nzhou/git')
-#from Ontology.IO import OboIO
-print os.getcwd()
 sys.path.append('/home/nzhou/git')
-from CAFAAssess.precrec.GOPred import GOPred
-
+from collections import defaultdict
+import numpy
 
 class benchmark:
     def __init__(self,ancestor_path,benchmark_path):
@@ -64,7 +56,23 @@ class benchmark:
                 #A: what does |= do?
                 self.true_terms[protein] |= ancestors
         
-    
+
+def read_benchmark(namespace):
+    if namespace=='BPO':
+        ancestor_path = './CAFAAssess/precrec/gene_ontology_edit.obo_ancestors_bpo.txt'
+        benchmark_path = './CAFAAssess/precrec/leafonly_BPO.txt'
+    elif namespace=='MFO':
+        ancestor_path = './CAFAAssess/precrec/gene_ontology_edit.obo_ancestors_mfo.txt'
+        benchmark_path = './CAFAAssess/precrec/leafonly_MFO.txt'
+    elif namespace =='CCO':
+        ancestor_path = './CAFAAssess/precrec/gene_ontology_edit.obo_ancestors_cco.txt'
+        benchmark_path = './CAFAAssess/precrec/leafonly_CCO.txt'
+    else:
+        raise ValueError('Please enter a valid ontology: BPO, MFO, CCO')
+    bench = benchmark(ancestor_path,benchmark_path)
+    bench.propagate()
+    return bench
+
 
 class PrecREC:
     '''
@@ -75,7 +83,7 @@ class PrecREC:
     read in ONTOLOGY-SPECIFIC prediction file
     #Do we need ONTOLOGY-SPECIFIC ontology file??
     '''
-    def __init__(self, benchmark, GOPred):
+    def __init__(self, benchmark, GoPred):
         '''
         constructor
         benchmark is an instance of the benchmark class
@@ -88,7 +96,7 @@ class PrecREC:
         self.counta = defaultdict()
         self.countb = 0
         
-        #predicted_base_terms is the same as GOPred.data
+        #predicted_base_terms is the same as GoPred.data
         #No need creating another dictionary
         
         #Now propogate the predicted terms
@@ -101,14 +109,14 @@ class PrecREC:
         
         #
         self.predicted = defaultdict(defaultdict)
-        for prot in GOPred.data:
+        for prot in GoPred.data:
             if benchmark.true_terms[prot]!=set():
                 '''
                 The protein is in the benchmark file
                 i.e. gained experimental annotation
                 '''
                 self.countb += 1
-                for tc in GOPred.data[prot]:
+                for tc in GoPred.data[prot]:
                     try:
                         ancterms = self.ancestors[tc['term']]
                     except KeyError:
@@ -278,90 +286,3 @@ class PrecREC:
         
 
 
-
-
-if __name__=='__main__':
-    '''
-    ancestor_path = '/home/nzhou/git/CAFAAssess/precrec/gene_ontology_edit.obo_ancestors_bpo.txt'  
-    #This benchmark is the full BPO benchmark
-    #benchmark_path = '/home/nzhou/git/CAFAAssess/precrec/leafonly_BPO.txt'
-    #bench = benchmark(ancestor_path,benchmark_path)
-    #bench.propagate()
-    #This benchmark is the human only BPO benchmark
-    hs = benchmark(ancestor_path,'/home/nzhou/git/CAFAAssess/precrec/leafonly_BPO_9606.txt')
-    hs.propagate()
-    
-    all_pred = GOPred()
-    pred_path ='/home/nzhou/git/CAFAAssess/confidence/117/PaccanaroLab_1_9606_BPO.txt'
-    all_pred.read(open(pred_path))
-    #b = PrecREC(bench,all_pred)
-    c = PrecREC(hs,all_pred)
-    #c.printConfidence('/home/nzhou/git/CAFAAssess/confidence/117/newconfdata.txt')
-    #print b.precision_recall(0.1)
-    print c.precision_recall(0.1)
-    #print b.getNumProteins(0.1)
-    #print c.getNumProteins(0.1)
-    fm = c.Fmax_output(9)
-    plt.plot(fm[0],fm[1],'ro')
-    plt.axis([0,1,0,1])
-    plt.show()
-    '''
-    '''
-    get confidence files for all human predictions for all top ten teams (model 1?)
-    in confidence.py
-    '''
-    '''
-    Get PR curve and Fmax for all top-performing teams in three ontologies
-    '''
-    bpo = ('/117/PaccanaroLab_1_9606_BPO.txt','/85/tianlab_1_9606_BPO.txt','/129/tu_1_9606_BPO.txt','/127/txt_BPO.txt','/94/txt_BPO.txt','/83/bonneauLab_1_9606_BPO.txt','/75/Homo_sapiens_BPO.txt','/Naive/BN4S_bpo.txt','/BLAST/BB4S_bpo.txt')
-    mfo = ('/129/tu_1_9606_MFO.txt','/72/EVEX_1_9606_MFO.txt','/85/tianlab_1_9606_MFO.txt','/75/Homo_sapiens_MFO.txt','/127/txt_MFO.txt','/101/GO2Proto_1_9606_0_MFO.txt','/102/brennersifter_1_9606_0_MFO.txt','/94/txt_MFO.txt','/Naive/BN4S_mfo.txt','/BLAST/BB4S_mfo.txt')
-    cco = ('/72/EVEX_1_9606_CCO.txt','/85/tianlab_1_9606_CCO.txt','/129/tu_1_9606_CCO.txt','/115/Homo_sapiens_CCO.txt','/83/bonneauLab_1_9606_CCO.txt','/86/IASLAcademiaSinica_1_9606_CCO.txt','/127/txt_CCO.txt','/Naive/BN4S_cco.txt','/BLAST/BB4S_cco.txt')
-    
-    #before running this function, make sure that a benchmark instance has been created and propagated.
-    def getplot(filename):
-        fullpath = '/home/nzhou/git/CAFAAssess/confidence'+filename
-        all_pred = GOPred()
-        all_pred.read(open(fullpath))
-        c = PrecREC(hs,all_pred)
-        fm = c.Fmax_output(99)
-        
-        plt.plot(fm[1],fm[0])
-        plt.axis([0,1,0,1])
-        plt.yticks(numpy.arange(0,1,0.1))
-        plt.xlabel('Recall')
-        plt.ylabel('Precision')
-        plt.title(fullpath.split('/')[-1].split('.')[0])
-        plt.savefig(fullpath.split('.')[0]+'.png')
-        plt.close()
-        return fm[2]
-        
-    #BPO
-    ancestor_path = '/home/nzhou/git/CAFAAssess/precrec/gene_ontology_edit.obo_ancestors_bpo.txt'  
-    benchmark_path =  '/home/nzhou/git/CAFAAssess/precrec/leafonly_BPO.txt'
-    hs = benchmark(ancestor_path, benchmark_path)
-    hs.propagate()
-    bpofm = defaultdict()
-    for i in bpo:
-        bpofm[i] = getplot(i)
-        
-    #MFO
-    ancestor_path = '/home/nzhou/git/CAFAAssess/precrec/gene_ontology_edit.obo_ancestors_mfo.txt'  
-    benchmark_path =  '/home/nzhou/git/CAFAAssess/precrec/leafonly_MFO.txt'
-    hs = benchmark(ancestor_path, benchmark_path)
-    hs.propagate()
-    mfofm = defaultdict()
-    for j in mfo:
-        mfofm[j]=getplot(j)
-    #CCO
-    ancestor_path = '/home/nzhou/git/CAFAAssess/precrec/gene_ontology_edit.obo_ancestors_cco.txt'  
-    benchmark_path =  '/home/nzhou/git/CAFAAssess/precrec/leafonly_CCO.txt'
-    hs = benchmark(ancestor_path, benchmark_path)
-    hs.propagate()
-    ccofm = defaultdict()
-    for k in cco:
-        ccofm[k]=getplot(k)
-        
-
-                    
-    
-    
